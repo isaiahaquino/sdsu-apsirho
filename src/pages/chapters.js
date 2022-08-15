b4671a0eb68d89d0ddc75dd28ca43fd92aa4d850
isaiahaquino <isaiahaquino1997@gmail.com>
2022-08-14 22:42:36 -1000

    Chapters page

--- src/pages/chapters.js
+++ src/pages/chapters.js
@@ -1,13 +1,49 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import '../styles/chaptersPage.css'
+import { graphql } from 'gatsby'
 
-function ChaptersPage() {
+function ChaptersPage({ data }) {
   return (
     <Layout>
+      <div className='chapters-main pageMain'>
+        <h1 className='pageTitle'>CHAPTERS</h1>
+        <p className='para'>{data.contentfulChaptersPage.paragraph.paragraph}</p>
+        <div className='chapters-container'>
+          {
+            data.allContentfulChapters.nodes.map(node => (
+                <div className='chapter' key={node.chapter}>
+                  <a href={node.url}><h1 className='pageSubtitle'>{node.chapter} ({node.status})</h1></a>
+                  <p>{node.location}</p>
+                  <p>{node.dateEstablished}</p>
+                </div>
+            ))
+          }
+        </div>
+      </div>
 
     </Layout>
   )
 }
+
+export const query = graphql`
+query {
+  contentfulChaptersPage {
+    paragraph {
+      paragraph
+    }
+  }
+  allContentfulChapters {
+    nodes {
+      chapter
+      url
+      location
+      dateEstablished
+      status
+    }
+  }
+}`
+
+
 
 export default ChaptersPage
